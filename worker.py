e01445d5bf0025239941d6b72deb2252c386b945
Diego Caples <diegocaples@gmail.com>
2024-08-19 18:29:25 -0700

    default run script, not notebook

--- worker.py
+++ worker.py
@@ -1,22 +1,27 @@
 import argparse
-# from train import train
+import train
 import wandb
 
-from activated_notebook_importer import import_notebook
 
-# Import a notebook
-trainer_module = import_notebook('train.ipynb')
-
+run_method = 'script'
 
 def run_worker(sweep_id, project):
-    # Initialize a new wandb run
-    wandb.agent(sweep_id, function=trainer_module.train, project=project)
+    train_function = train.train
+
+    if run_method == 'notebook':  
+        from activated_notebook_importer import import_notebook
+
+        # Import the train function from the notebook.
+        trainer_module = import_notebook('train.ipynb')
+        train_function = trainer_module.train
+
+    wandb.agent(sweep_id, function=train_function, project=project)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Run a WandB sweep worker")
     parser.add_argument("-s", "--sweep_id", help="The ID of the WandB sweep to run", required=True)
     parser.add_argument("-p", "--project", help="The name of the WandB project", required=True)
-    parser.add_argument('-n", ')
     args = parser.parse_args()
 
     # Login to WandB
